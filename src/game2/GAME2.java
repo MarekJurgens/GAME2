f68adb73801dd6a3d048a77b34241156e3fe7b8f
MarekJurgens <marek.jurgens@ivkhk.ee>
2021-09-13 11:10:52 +0300

    DOBAVIL COD

--- src/game2/GAME2.java
+++ src/game2/GAME2.java
@@ -5,6 +5,9 @@
  */
 package game2;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -15,7 +18,28 @@ public class GAME2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int attempt = 0;
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(9-0+1)+0;
+        System.out.print("Программа запомнила число от 0 до 9, угадай! ");
+    //    for(int i = 0; i < 3; i++){
+        while(true){
+            int userNumber = scanner.nextInt();
+            if(number == userNumber){
+                System.out.println("Ура! Ты выиграл!!!");
+                break;
+            }else{
+                if(attempt < 2){
+                    System.out.println(":(, неправильно. Попробуй еще раз: ");
+                }else{
+                    System.out.println(":(, ты проиграл");
+                    break;
+                }
+            } 
+            attempt++;
+        }
+    
     }
     
 }
